b1d6cfc78227bd6d79fdb45a35a7a477f93d5b84
jakub-swiniarski <jakub-swiniarski@protonmail.com>
2024-04-12 21:45:33 +0200

    no more magic numbers

--- src/InputProcessor.cpp
+++ src/InputProcessor.cpp
@@ -3,17 +3,19 @@
 #include "Ball.hpp"
 #include "InputProcessor.hpp"
 
+#include "config.hpp"
+
 InputProcessor::InputProcessor(Ball *p) {
     player = p;
 }
 
 void InputProcessor::update(float mod) {
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W))
-        player->move(0.f, -100.f * mod);
+        player->move(0.f, -BALL_SPEED * mod);
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S))
-        player->move(0.f, 100.f * mod);
+        player->move(0.f, BALL_SPEED * mod);
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A))
-        player->move(-100.f * mod, 0.f);
+        player->move(-BALL_SPEED * mod, 0.f);
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D))
-        player->move(100.f * mod, 0.f);
+        player->move(BALL_SPEED * mod, 0.f);
 }
