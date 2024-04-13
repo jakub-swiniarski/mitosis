bf73d0fe85c9bf7a0e24e194128fafdd89a8ddab
jakub-swiniarski <jakub-swiniarski@protonmail.com>
2024-04-13 20:30:15 +0200

    commit food

---
+++ src/Food.cpp
@@ -0,0 +1,19 @@
+#include <SFML/Graphics/Color.hpp>
+#include <cmath>
+#include <random>
+
+#include "Food.hpp"
+
+#include "config.hpp"
+
+Food::Food(float player_x, float player_y) {
+    std::random_device dev;
+    std::mt19937 rng(dev());
+    std::uniform_int_distribution<std::mt19937::result_type> rand(0, 360);
+    float rotation = rand(rng) * M_PI / 180.f;
+    float distance = std::sqrt(std::pow(0.5 * cfg::Window::WIDTH, 2) + std::pow(0.5 * cfg::Window::HEIGHT, 2));
+
+    setRadius(10.f);
+    setFillColor(sf::Color::Blue);
+    setPosition(player_x + distance * cos(rotation), player_y + distance * sin(rotation)); 
+}
---
+++ src/Food.hpp
@@ -0,0 +1,11 @@
+#ifndef FOOD_HPP
+#define FOOD_HPP
+
+#include <SFML/Graphics/CircleShape.hpp>
+
+class Food: public sf::CircleShape {
+public:
+    Food(float player_x, float player_y);
+};
+
+#endif /* FOOD_HPP */
