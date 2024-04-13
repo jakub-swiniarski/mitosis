055ba2551c468f146f0104e33e9f8ce730a53186
jakub-swiniarski <jakub-swiniarski@protonmail.com>
2024-04-13 22:37:11 +0200

    just for safety

--- src/Food.cpp
+++ src/Food.cpp
@@ -11,7 +11,7 @@ Food::Food(float player_x, float player_y) {
     std::mt19937 rng(dev());
     std::uniform_int_distribution<std::mt19937::result_type> rand(0, 360);
     float rotation = rand(rng) * M_PI / 180.f;
-    float distance = std::sqrt(std::pow(0.5 * cfg::Window::WIDTH, 2) + std::pow(0.5 * cfg::Window::HEIGHT, 2)) + 4.f * cfg::Food::RADIUS;
+    float distance = std::sqrt(std::pow(0.5 * cfg::Window::WIDTH, 2) + std::pow(0.5 * cfg::Window::HEIGHT, 2)) + 5.f * cfg::Food::RADIUS;
 
     setRadius(cfg::Food::RADIUS);
     setFillColor(sf::Color::Blue);
