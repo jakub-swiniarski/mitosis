38cd36dbbe653d1edfee14df1243858ec21284b4
Jakub Świniarski <77209709+jakub-swiniarski@users.noreply.github.com>
2024-04-15 20:53:26 +0200

    random food color

--- src/Food.cpp
+++ src/Food.cpp
@@ -9,11 +9,12 @@
 Food::Food(float player_x, float player_y) {
     std::random_device dev;
     std::mt19937 rng(dev());
-    std::uniform_int_distribution<std::mt19937::result_type> rand(0, 360);
-    float rotation = rand(rng) * M_PI / 180.f;
+    std::uniform_int_distribution<std::mt19937::result_type> rand_rotation(0, 360);
+    std::uniform_int_distribution<std::mt19937::result_type> rand_rgb(0, 255);
+    float rotation = rand_rotation(rng) * M_PI / 180.f;
     float distance = std::sqrt(std::pow(0.5 * cfg::window::WIDTH, 2) + std::pow(0.5 * cfg::window::HEIGHT, 2)) + 5.f * cfg::food::RADIUS;
 
     setRadius(cfg::food::RADIUS);
-    setFillColor(sf::Color::Blue);
+    setFillColor(sf::Color(rand_rgb(rng), rand_rgb(rng), rand_rgb(rng)));
     setPosition(player_x + distance * cos(rotation), player_y + distance * sin(rotation)); 
 }
