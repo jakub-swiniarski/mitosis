0cffd7c372e24d44cbdb3ae41db7c1592a5911cc
jakub-swiniarski <jakub-swiniarski@protonmail.com>
2024-04-22 19:31:04 +0200

    simplify get_middle

--- src/Disk.cpp
+++ src/Disk.cpp
@@ -3,8 +3,10 @@
 #include "Disk.hpp"
 
 sf::Vector2f Disk::get_middle(void) const {
-    sf::Vector2f middle(getPosition().x + getRadius(), getPosition().y + getRadius());
-    return middle;
+    return sf::Vector2f(
+        getPosition().x + getRadius(),
+        getPosition().y + getRadius()
+    );
 }
 
 float Disk::get_distance(float x, float y) const {
