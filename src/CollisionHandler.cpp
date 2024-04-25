aa91091fe7b9540b2ffa2e3373bf0d725f4c70cf
Jakub Świniarski <77209709+jakub-swiniarski@users.noreply.github.com>
2024-04-25 18:41:02 +0200

    this is a mess

---
+++ src/CollisionHandler.cpp
@@ -0,0 +1,27 @@
+#include <forward_list>
+
+#include "CollisionHandler.hpp"
+#include "Food.hpp"
+#include "Cell.hpp"
+
+CollisionHandler::CollisionHandler(Cell *e, Cell *p, std::forward_list<Food> *f) {
+    enemy = e;
+    player = p;
+    food = f;
+}
+
+void CollisionHandler::update(void) {
+    auto prev = food->before_begin();
+    for (auto i = food->before_begin(); i != food->end(); i++) {
+        if (i->collision(player)) {
+            player->grow(i->getRadius());
+            food->erase_after(prev);
+            break;
+        } else if (i->collision(enemy)) {
+            enemy->grow(i->getRadius());
+            food->erase_after(prev);
+            break;
+        }
+        prev = i;
+    }
+}
