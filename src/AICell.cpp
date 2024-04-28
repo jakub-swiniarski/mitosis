375fbec4a692606e2e08476cce5f3742148b4970
jakub-swiniarski <jakub-swiniarski@protonmail.com>
2024-04-28 18:29:47 +0200

    eliminate magic numbers

--- src/AICell.cpp
+++ src/AICell.cpp
@@ -4,7 +4,7 @@
 
 #include "config.hpp"
 
-AICell::AICell(sf::Vector2f v) : Cell(v), timer(2.f) {};
+AICell::AICell(sf::Vector2f v) : Cell(v), timer(cfg::ai_cell::ai_cooldown) {};
 
 void AICell::update(float mod) {
     if (timer.elapsed()) {
