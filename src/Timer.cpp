69db77dd8740d06142b69222b79f56cd5d39b514
jakub-swiniarski <jakub-swiniarski@protonmail.com>
2024-04-28 16:58:32 +0200

    timer class

---
+++ src/Timer.cpp
@@ -0,0 +1,13 @@
+#include "Timer.hpp"
+
+Timer::Timer(float c) {
+    cooldown = c;
+}
+
+bool Timer::elapsed(void) {
+    if(getElapsedTime().asSeconds() >= cooldown) {
+        restart();
+        return 1;
+    }
+    return 0;
+}
---
+++ src/Timer.hpp
@@ -0,0 +1,14 @@
+#ifndef TIMER_HPP
+#define TIMER_HPP
+
+#include <SFML/System/Clock.hpp>
+
+class Timer : private sf::Clock {
+    float cooldown;
+
+public:
+    Timer(float c);
+    bool elapsed(void);
+};
+
+#endif /* TIMER_HPP */
