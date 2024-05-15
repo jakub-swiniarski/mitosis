27c8da1a7553f049894195151808533fa29546e3
Jakub Świniarski <77209709+jakub-swiniarski@users.noreply.github.com>
2024-05-15 18:23:18 +0200

    consume cells, needs some work

--- src/CollisionHandler.cpp
+++ src/CollisionHandler.cpp
@@ -5,8 +5,21 @@
 CollisionHandler::CollisionHandler(std::list<Cell> *c, std::list<Food> *f) : cells(c), food(f) {}
 
 void CollisionHandler::update(void) {
-    for (auto f = food->begin(); f != food->end(); f++) {
-        for (auto c = cells->begin(); c != cells->end(); c++) {
+    for (auto c = cells->begin(); c != cells->end(); c++) {
+        for (auto c1 = cells->begin(); c1 != cells->end(); c1++) {
+            if (c != c1 && c->collision(&(*c1))) {
+                if (c->getRadius() > c1->getRadius()) {
+                    c->grow(c1->getRadius());
+                    cells->erase(c1);
+                } else {
+                    c1->grow(c->getRadius());
+                    cells->erase(c);
+                }
+                return;
+            }
+        }
+
+        for (auto f = food->begin(); f != food->end(); f++) {
             if (f->collision(&(*c))) {
                 c->grow(f->getRadius());
                 food->erase(f);
