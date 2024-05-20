5440eeda7b7d8f5e45d41ac56969186e6413757e
Jakub Świniarski <77209709+jakub-swiniarski@users.noreply.github.com>
2024-05-20 18:14:49 +0200

    add new file

---
+++ src/DrawableContainer.hpp
@@ -0,0 +1,16 @@
+#ifndef DRAWABLECONTAINER_HPP
+#define DRAWABLECONTAINER_HPP
+
+#include <list>
+
+class Cell;
+class Food;
+class Counter;
+
+struct DrawableContainer {
+    std::list<Cell> *cells;
+    std::list<Food> *food;
+    Counter *counter;
+};
+
+#endif /* DRAWABLECONTAINER_HPP */
