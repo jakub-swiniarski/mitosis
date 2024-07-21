f01a3b30215a3643337ce232f4540427a0641491
Jakub Świniarski <77209709+jakub-swiniarski@users.noreply.github.com>
2024-07-21 16:44:10 +0200

    style changes

--- src/Food.cpp
+++ src/Food.cpp
@@ -10,8 +10,8 @@ Food::Food(sf::Vector2f player_position) {
     std::mt19937 rng(dev());
     std::uniform_int_distribution<std::mt19937::result_type> rand_rotation(0, 360);
     std::uniform_int_distribution<std::mt19937::result_type> rand_rgb(0, 255);
-    float rotation = rand_rotation(rng) * M_PI / 180.f;
-    float distance = std::sqrt(std::pow(0.5 * cfg::window::width, 2) + std::pow(0.5 * cfg::window::height, 2)) + 5.f * cfg::food::radius;
+    float rotation = rand_rotation(rng) * M_PI / 180.0f;
+    float distance = std::sqrt(std::pow(0.5f * cfg::window::width, 2) + std::pow(0.5f * cfg::window::height, 2)) + 5.0f * cfg::food::radius;
 
     setRadius(cfg::food::radius);
     setFillColor(sf::Color(rand_rgb(rng), rand_rgb(rng), rand_rgb(rng)));
